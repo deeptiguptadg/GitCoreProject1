50cf1171477d353eda6b5ccdd7e8bccfe2d59db1
deept <deept@DESKTOP-VL96N7H>
2021-06-08 18:21:22 +0530

    Commit massege add deepti test java and upadte test

---
+++ src/com/DeeptiTest.java
@@ -0,0 +1,10 @@
+package com;
+
+public class DeeptiTest {
+	
+	public static void main(String[] agr)
+	{
+		System.out.println("hello deepit Developer");
+	}
+
+}
--- src/com/Test.java
+++ src/com/Test.java
@@ -2,9 +2,12 @@ package com;
 
 public class Test {
 	
+	
 	public static void main(String[] agr)
 	{
 		System.out.println("hello github");
+		System.out.println("hello git devep");
+
 	}
 
 }
